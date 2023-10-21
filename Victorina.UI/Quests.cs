d0d8311d4a6590fe56beb86badce36460d582218
pollosatyi <88507116+pollosatyi@users.noreply.github.com>
2023-10-21 20:39:05 +0300

    последнее изменение

---
+++ Victorina.UI/Quests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Victorina.UI
+{
+    public  class Quests
+    {
+        bool isRun = true;
+
+        while (isRun)
+        {
+            Console.WriteLine("выбрать действие: ");
+            Console.WriteLine("1- создать викторину ");
+            Console.WriteLine("2- выбрать викторину ");
+            Console.WriteLine("3- выйти ");
+
+            StartMenuEnum userInput;
+            try
+            {
+                userInput = (StartMenuEnum) Convert.ToInt32(Console.ReadLine());
+
+            }
+            catch
+            {
+                Console.Clear();
+                continue;
+            }
+        }
+
+    }
+ 
+}
---
+++ VictorinaUI/Program.cs
@@ -0,0 +1,39 @@
+﻿bool isRun = true;
+
+while (isRun)
+{
+    Console.WriteLine("выбрать действие: ");
+    Console.WriteLine("1- создать викторину ");
+    Console.WriteLine("2- выбрать викторину ");
+    Console.WriteLine("3- выйти ");
+
+    StartMenuEnum userInput;
+    try
+    {
+        userInput = (StartMenuEnum)Convert.ToInt32(Console.ReadLine());
+
+    }
+    catch
+    {
+        Console.Clear();
+        continue;
+    }
+
+    switch (userInput)
+    {
+        case StartMenuEnum.СreateQuiz:
+
+            break;
+        case StartMenuEnum.ChoizeQuiz:
+
+            break;
+        case StartMenuEnum.Exit:
+            isRun = false;
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("Неизвестная enum");
+            Console.WriteLine();
+            break;
+    }
+}
