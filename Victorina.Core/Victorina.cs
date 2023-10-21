7a8bd6c6f237ba1ab81502e530ca595f2169d6d6
pollosatyi <88507116+pollosatyi@users.noreply.github.com>
2023-10-21 18:46:45 +0300

    Изменение после извлечения от ветки

---
+++ Victorina.Core/Victorina.cs
@@ -0,0 +1,16 @@
+﻿namespace Victorina.Core
+{
+    public class Victorina
+    {
+       
+            public string Name { get; set; }
+            public int BookCount { get; set; }
+
+            public V(string name, int bookCount)
+            {
+                Name = name;
+                BookCount = bookCount;
+            }
+        }
+    
+}
