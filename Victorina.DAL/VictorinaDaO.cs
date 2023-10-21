82d7d3fc80dccb7b3048dc109a8a944c9c4afe76
pollosatyi <88507116+pollosatyi@users.noreply.github.com>
2023-10-21 18:14:32 +0300

    Создание dal

---
+++ Victorina.DAL/VictorinaDaO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Victorina.DAL
+{
+    public class VictorinaDaO : IVictoryDaO
+    {
+        private const string PATH = "D:\\Library\\library.json";
+    }
+}
