cc49c9137573ecb1dece3f8cad876b49b9255dcb
student <student@4PC8>
2023-10-21 20:02:02 +0300

    1

--- Victorina.UI/VictorinaUI.cs
+++ Victorina.UI/VictorinaUI.cs
@@ -1,11 +1,37 @@
-﻿namespace Victorina.UI
-{
-    public static class VictorinaUI
-    {
-        public static void Victorina()
-        {
-            Console.WriteLine("Введите количество викторин:");
+﻿using Victorina.UI;
 
-        }
+bool isRun = true;
+
+while (isRun)
+{
+    Console.WriteLine("выбрать действие: ");
+    Console.WriteLine("1- выбрать викторину ");
+    Console.WriteLine("2- выйти ");
+
+    StartMenuEnum userInput;
+    try
+    {
+        userInput = (StartMenuEnum)Convert.ToInt32(Console.ReadLine());
+
+    }
+    catch
+    {
+        Console.Clear();
+        continue;
+    }
+
+    switch (userInput)
+    {
+        case StartMenuEnum.ChoizeQuiz:
+            VictorinaUI.Victorina();
+            break;
+        case StartMenuEnum.Exit:
+            isRun = false;
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("Неизвестная enum");
+            Console.WriteLine();
+            break;
     }
 }
