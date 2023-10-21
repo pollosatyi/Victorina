8d1fa18ac5223aeeab41cc7a177fbfe93a4be3c5
pollosatyi <88507116+pollosatyi@users.noreply.github.com>
2023-10-21 18:34:07 +0300

    Доработка DLL

--- Victorina.DAL/VictorinaDaO.cs
+++ Victorina.DAL/VictorinaDaO.cs
@@ -2,12 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Victorina.DAL
 {
     public class VictorinaDaO : IVictoryDaO
     {
-        private const string PATH = "D:\\Library\\library.json";
+        private const string PATH = "D:\\sergey t\\Victorina\\VictorinaJson\\victorina.json";
+
+        public void Create(Victorina victorina)
+        {
+            var victorinas = new List<Victorina>();
+
+            if (File.Exists(PATH))
+            {
+                using (FileStream fs = new FileStream(PATH, FileMode.OpenOrCreate))
+                {
+                    victorinas = JsonSerializer.Deserialize<List<Victorina>>(fs);
+                }
+            }
+
+            victorinas.Add(victorina);
+            Write(libraries);
+        }
+
+        private void Write(List<Victorina> victorina)
+        {
+            using (FileStream fs = new FileStream(PATH, FileMode.OpenOrCreate))
+            {
+                JsonSerializer.Serialize<List<Victorina>>(fs, victorina);
+            }
+        }
     }
 }
