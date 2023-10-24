bb0fe1ec7a9ce7d689dd478947f7d63c78b97040
pollosatyi <pollosatyi@gmail.com>
2023-10-24 05:04:41 +0300

    Создание класса Quiz

--- Victoria.BLL/VictorinaService.cs
+++ Victoria.BLL/VictorinaService.cs
@@ -9,11 +9,37 @@ namespace Victoria.BLL
         private readonly IVictoryDaO _victoryDao = new VictorinaDaO();
         public void Create()
         {
-            var quest= new Quiz();
-            _victoryDao.Create(quest);
+            var quiz= new Quiz(InitQuestion(),InitAnswers());
+            _victoryDao.Create(quiz);
         }
 
-        public string Qu
+        public string InitQuestion()
+        {
+            string question = "";
+            Console.WriteLine("Введите вопрос: ");
+            question = Console.ReadLine();
+            return question;
+        }
+
+        public Dictionary<bool,string> InitAnswers()
+        {
+            Dictionary<bool, string> answers = new Dictionary<bool, string>();
+            for(int i = 0; i < 4; i++)
+            {
+                string answer="";
+                int check = 0;
+                bool IsRightAnswer;
+                Console.WriteLine("Введите ответ: ");
+                answer = Console.ReadLine();
+                Console.WriteLine("Если ответ неверный нажмите 0 или любую цифру если верный ");
+                check = int.Parse(Console.ReadLine());
+                IsRightAnswer = (check!=0)?(true):(false);
+                answers.Add(IsRightAnswer,answer);
+            }
+            return answers;
+        }
+
+       
     }
 
 }
--- Victorina.Core/Victorina.cs
+++ Victorina.Core/Victorina.cs
@@ -6,9 +6,10 @@
             public string _question { get; set; }
         public Dictionary<bool, string> _answers = new Dictionary<bool, string>();
 
-            public Quiz(string question,Dictionary<bool,string> answers)
+            public Quiz(string question,Dictionary<bool,string>answers)
             {
-
+            _question= question;
+            _answers = answers;
                 
             }
         }
